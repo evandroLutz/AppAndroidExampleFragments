e38982145c4edd56fb83f7727a4d32a94e97031a
Evandro Lutz <evandrolutz@outlook.com>
2021-12-19 22:59:09 -0300

    insere edicao de usuario

--- app/src/main/java/com/example/entrega3/fragments/EditarFragment.java
+++ app/src/main/java/com/example/entrega3/fragments/EditarFragment.java
@@ -2,41 +2,34 @@ package com.example.entrega3.fragments;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
+import com.example.entrega3.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
-import com.example.entrega3.R;
-
 
 public class EditarFragment extends Fragment {
-    private TextInputEditText txtNome;
-    private TextInputEditText txtDtNasc;
-    private TextInputEditText txtEnd;
+    private AppCompatEditText txtNome;
     private Button btnEditar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_editar, container, false);
-        Bundle bundle = getArguments();
-        String nome = bundle.getString("NOME");
-        txtNome = root.findViewById(R.id.txtNome);
-        txtNome.setText(nome);
-        txtEnd = root.findViewById(R.id.txtEnd);
-        txtEnd.setText(bundle.getString("END"));
-        txtDtNasc = root.findViewById(R.id.txtDtNasc);
-        txtDtNasc.setText(bundle.getString("DTNASC"));
-        btnEditar = root.findViewById(R.id.btnEditar);
+        View root = inflater.inflate(R.layout.fragment_cadastra, container, false);
+        txtNome = root.findViewById(R.id.editTextnomeUsuario);
+        btnEditar = root.findViewById(R.id.btnCadUsuario);
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,14 +40,15 @@ public class EditarFragment extends Fragment {
     }
     public void editarItem() {
         new AlertDialog.Builder(getContext())
-                .setTitle("Editando pessoa")
-                .setMessage("Tem certeza que deseja editar essa pessoa?")
+                .setTitle("O usuário esta sendo editado")
+                .setMessage("Deseja proceder com a edição do usuário?")
                 .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Snackbar.make(getView(), "item editado!!!", Snackbar.LENGTH_LONG).show();
+                        Snackbar.make(getView(), "O usuário foi editado!!!", Snackbar.LENGTH_LONG).show();
                         Navigation.findNavController(getView()).navigate(R.id.action_nav_editarFragment_to_nav_listarFragment);
                     }
                 }).setNegativeButton("Não", null).show();
+
     }
 }
